59d6bb0b1f2bd0a1748418927716c42ca7a94c90
D.Okanome <Deanokanome@hotmail.com>
2019-01-27 18:44:37 -0500

    bind syntax

--- src/App.js
+++ src/App.js
@@ -6,14 +6,41 @@ import Hobbies from './Hobbies/Hobbies'
 
 
 class App extends Component {
+  state = {
+    persons: [
+      { name:"Max" , age:"28" },
+      { name:"James" , age:"25" },
+    ],
+    otherState: 'some other value'
+  }
+  
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    this.setState({
+      persons: [
+      { name:newName , age:"28" },
+      { name:"Jamey" , age:"50" },
+    ] 
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, im a React App</h1>
         <p>This is really working</p>
-        <Person name="Max" age="28"/>
-        <Hobbies sports="basketball"/>
-      </div>
+        <button onClick={this.switchNameHandler.bind(this, 'Maxim')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}/>My Hobbies are Basketball
+          />
+        <Hobbies sports="football"/>
+        </div>
     );
     // return React.createElement('div', null, React.createElement('h1', {className:'App'}, 'Does this work now?'));
   }
