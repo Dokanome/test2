0eb2b336bb40915543e4250a0d97d5fba4384b70
D.Okanome <Deanokanome@hotmail.com>
2019-01-30 22:08:07 -0500

    show person

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Hobbies from './Hobbies/Hobbies'
 
 
 
@@ -34,11 +33,21 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'green',
+      font: 'inherit',
+      border: '5px solid green',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, im a React App</h1>
         <p>This is really working</p>
-        <button onClick={() => this.switchNameHandler('Maxim!!')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Maxim!!')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}
@@ -48,8 +57,7 @@ class App extends Component {
           age={this.state.persons[1].age}
           click={this.switchNameHandler.bind(this, 'Max!')}
           changed={this.nameChangedHandler}/>My Hobbies are Basketball
-          />
-        <Hobbies sports="football"/>
+
         </div>
     );
     // return React.createElement('div', null, React.createElement('h1', {className:'App'}, 'Does this work now?'));
