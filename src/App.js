d3bc5921f62aeb46051683694d0347ae834c5b18
D.Okanome <Deanokanome@hotmail.com>
2019-02-04 17:50:15 -0500

    deletehandler

--- src/App.js
+++ src/App.js
@@ -14,14 +14,11 @@ class App extends Component {
     showPersons: false
   }
   
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    this.setState({
-      persons: [
-        { name:newName , age:"28" },
-        { name:"Jamey" , age:"50" },
-      ] 
-    } )
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({person: persons})
   }
 
   nameChangedHandler = (event) => {
@@ -52,15 +49,12 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}
-            />
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangedHandler}/>My Hobbies are Basketball
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            click= {() => this.deletePersonHandler(index)}
+            name= {person.name} 
+            age= {person.age} />
+          })}
           </div> 
       );
     }
